648a8d34cdad96c4aa318c23a8628305b432903b
Nathan Fritzler <nfblaster@live.com>
2019-08-18 17:52:30 -0600

    Add a bit more unit testing.

--- demo/main.js
+++ demo/main.js
@@ -1,49 +1,68 @@
 (function () {
-	var cnvs = document.getElementById("win");
+	var cnvs = document.getElementById("win"),
+		ctx=cnvs.getContext("2d");
 	window.onresize = function () {
 		cnvs.width = window.innerWidth;
 		cnvs.height = window.innerHeight;
 	}
 	window.onresize();
 
-	var passed=0,
-		tested=0,
-		failed=0;
-	function test(bool) {
+	var tested=0,
+		failed=0,
+		importantF=0;
+	function claim(name,actual,shouldbe,important,boolOverride) {
+		console.log(name,actual,"(should be",shouldbe,")");
 		tested++;
+		var bool=actual===shouldbe;
+		if (typeof boolOverride!=="undefined") bool=boolOverride(actual,shouldbe);
 		if (bool) {
 			console.info("PASS");
-			passed++;
+		}else if (typeof important==="undefined"||important){
+			console.error("CRITICAL FAIL");
+			failed++;
+			importantF++;
 		}else{
 			console.warn("FAIL");
 			failed++;
 		}
 	}
-	function claim(name,actual,shouldbe) {
-		console.log(name,actual,"(should be",shouldbe,")");
-		test(actual===shouldbe);
+	function gradeLetter(percentage) {
+		var gradeL="F";
+		/*if (percentage>=97) gradeL="A+";
+		else */if (percentage>=93) gradeL="A";
+		else if (percentage>=90) gradeL="A-";
+		else if (percentage>=87) gradeL="B+";
+		else if (percentage>=83) gradeL="B";
+		else if (percentage>=80) gradeL="B-";
+		else if (percentage>=77) gradeL="C+";
+		else if (percentage>=73) gradeL="C";
+		else if (percentage>=70) gradeL="C-";
+		else if (percentage>=67) gradeL="D+";
+		else if (percentage>=65) gradeL="D";
+		return gradeL;
 	}
 
-	console.group("Unit test of library");
+	console.groupCollapsed("Unit test of library");
+	
 	claim("The distance of (0,1) from the origin is",pc.distance(0,1),1);
-	claim("The the radian rotation of (-1,0) from (1,0) is",
-		pc.findRot(-1,0),Math.PI);
-	claim("",1,1);
+	claim("The the radian rotation of (0,1) from (1,0) is",
+		pc.findRot(0,1),Math.PI/2);
+	claim("The the radian rotation of (0,-1) from (1,0) is",
+		pc.findRot(0,-1),3*Math.PI/2,false);
+	claim("The x-position of the raw rotation of the point (0,1) pi radians is",
+		pc.rawRotate(-1,0,Math.PI)[0],-1);
+	claim("The x-position of the rotation of the point (0,1) pi radians is",
+		pc.rotate(-1,0,Math.PI)[0],1);
+	claim("The x-position of the rotation of the point (0,1) pi radians is",
+		pc.rotate(-1,0,Math.PI)[0],1);
+	
 	console.log("Tested:",tested);
-	console.log("Passed:",passed, "(",passed/tested*100,"%)");
+	console.log("Passed:",tested-failed, "(",(tested-failed)/tested*100,"%)");
 	console.log("Failed:",failed, "(",failed/tested*100,"%)");
+	console.log("Critical Failures:",failed, "(",importantF/tested*100,"%)");
 	console.groupEnd();
-	var gradeL="F",grade=passed/tested*100;
-	if (grade>=97) gradeL="A+";
-	else if (grade>=93) gradeL="A";
-	else if (grade>=90) gradeL="A-";
-	else if (grade>=87) gradeL="B+";
-	else if (grade>=83) gradeL="B";
-	else if (grade>=80) gradeL="B-";
-	else if (grade>=77) gradeL="C+";
-	else if (grade>=73) gradeL="C";
-	else if (grade>=70) gradeL="C-";
-	else if (grade>=67) gradeL="D+";
-	else if (grade>=65) gradeL="D";
-	console.info("Got a(n)",gradeL,"(",Math.round(grade),"%) in the unit test.");
+	var grade=(tested-failed)/tested*100,gradeL=gradeLetter(grade);
+	console.info("Got a(n)",gradeL,"(",Math.round(grade),"%) in the strict unit test.");
+	var grade=(tested-importantF)/tested*100,gradeL=gradeLetter(grade);
+	console.info("Got a(n)",gradeL,"(",Math.round(grade),"%) in the critical unit test.");
 })()
