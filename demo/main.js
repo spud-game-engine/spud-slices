a2fbda2f4725f8cf646496dfa21bf09dee6855ba
Nathan Fritzler <nfblaster@live.com>
2019-09-07 11:44:44 -0600

    remove demo dir

--- demo/main.js
+++
@@ -1,9 +0,0 @@
-(function () {
-	var cnvs = document.getElementById("win"),
-		ctx=cnvs.getContext("2d");
-	window.onresize = function () {
-		cnvs.width = window.innerWidth;
-		cnvs.height = window.innerHeight;
-	};
-	window.onresize();
-})();
